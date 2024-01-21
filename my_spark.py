de1f216f0978fcf174df8433d4e8272cf84faae1
vegetablek1d <95077785+vegetablek1d@users.noreply.github.com>
2024-01-21 14:03:54 +0300

    Update my_spark.py

--- my_spark.py
+++ my_spark.py
@@ -1,30 +1,27 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, avg
 
-# Создание Spark сессии
 spark = SparkSession.builder \
-    .appName("HousePricesAnalysis") \
+    .appName("(Не)движимость") \
     .config("spark.driver.extraClassPath", "/opt/bitnami/spark/jars/postgresql-42.7.0.jar") \
     .getOrCreate()
 
-# Загрузка данных из PostgreSQL таблицы
-jdbc_url = "jdbc:postgresql://postgres:5432/mydb"
+jdbc_url = "jdbc:postgresql://postgres:5432/houses"
 connection_properties = {
-    "user": "myuser",
-    "password": "mypassword",
+    "user": "user",
+    "password": "password",
     "driver": "org.postgresql.Driver"
 }
 
-house_prices_df = spark.read \
-    .jdbc(url=jdbc_url, table="house_prices", properties=connection_properties)
+df = spark.read.option("header", "true").csv("/opt/bitnami/spark/house_prices.csv")
 
-# Выполнение анализа данных
-result_df = house_prices_df.groupBy("location", "bedrooms") \
+print(df)
+
+
+result_df = df.groupBy("location", "bedrooms") \
     .agg(avg("price").alias("avg_price")) \
     .orderBy("location", "bedrooms")
 
-# Вывод результатов
 result_df.show()
 
-# Завершение Spark сессии
 spark.stop()
